80a43507c699fdd95a22989c2c507be5d69ea3a0
Bohdan <bohdanfiloenko657@gmail.com>
2024-01-22 10:12:31 +0200

    Added test output

--- test.py
+++ test.py
@@ -1,46 +1,14 @@
-from easy_pil import Canvas, Editor, Font
+import pandas as pd
+from tinydb import TinyDB, Query
 
-user_data = {
-    "name": "Shahriyar#9770",
-    "bio": "An Example Bot user",
-    "level": "15",
-    "xp": "1.2k / 3k",
-    "percentage": 45,
-}
+mdb = TinyDB('./json/marksdb.json', encoding='utf-8')
+User = Query()
 
+json_data = mdb.search(User.user_id == "<@996353781300215868>")
 
-background = Editor("images/profile_bg.png")
+data = [record for record in json_data]
+df = pd.DataFrame(data).reset_index()
+df = df.rename(columns={'index': 'ID'})
 
-# For profile to use users profile picture load it from url using the load_image/load_image_async function
-# profile_image = load_image(str(ctx.author.avatar_url))
-# profile = Editor(profile_image).resize((200, 200))
+df.to_excel('output.xlsx', index=False)
 
-
-font_40 = Font.poppins(size=40)
-font_20 = Font.montserrat(size=20)
-font_25 = Font.poppins(size=25)
-font_40_bold = Font.poppins(size=40, variant="bold")
-
-background.text((240, 20), user_data["name"], font=font_40, color="white")
-background.text((240, 80), user_data["bio"], font=font_20, color="white")
-background.text((250, 170), "LVL", font=font_25, color="white")
-background.text((310, 155), user_data["level"], font=font_40_bold, color="white")
-
-background.rectangle((390, 170), 360, 25, outline="white", stroke_width=2)
-background.bar(
-    (394, 174),
-    352,
-    17,
-    percentage=user_data["percentage"],
-    fill="white",
-    stroke_width=2,
-)
-
-background.text((390, 135), "Rank : 45", font=font_25, color="white")
-background.text(
-    (750, 135), f"XP : {user_data['xp']}", font=font_25, color="white", align="right"
-)
-user_id = "<@996353781300215868>"
-user_id_for_filename = str(user_id).replace("<", "").replace(">", "").replace("@", "")
-file_path = f'images/profile_image_{user_id_for_filename}.png'
-background.save(file_path)
